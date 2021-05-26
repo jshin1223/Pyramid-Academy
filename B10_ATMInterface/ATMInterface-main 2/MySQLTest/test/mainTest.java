10e1e9c4fc94b6732cb40267386163081d62487c
Joshua Shin <jshin1223@gmail.com>
2021-05-26 10:03:07 -0500

    test

---
+++ B10_ATMInterface/ATMInterface-main 2/MySQLTest/test/mainTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void transfer() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void printUserMenu() {
+    }
+
+    @Test
+    void getSavings() {
+    }
+
+    @Test
+    void getCheckings() {
+    }
+
+    @Test
+    void getUID() {
+    }
+}
